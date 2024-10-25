0966436faed1516c7cb89f258f0b32a0bf7585dd
ahnas <manuahnas@gmail.com>
2024-10-26 00:55:28 +0530

    Product page Design

--- app/routes/products.tsx
+++ app/routes/products.tsx
@@ -7,7 +7,7 @@ import Navbar from "~/components/Navbar";
 export const meta: MetaFunction = () => {
   return [
     { title: "Product" },
-    { name: "description", content: "Welcome to Remix!" },
+    { name: "description", content: "Welcome to Baby Pulse!" },
   ];
 };
 
@@ -36,33 +36,31 @@ export default function ProductsPage() {
       <h1 className="text-2xl font-bold mb-6">Product List</h1>
 
       <div className="flex flex-wrap justify-center">
-        {products.map((product) => (
-          <div key={product.id} className="bg-white w-64 m-5 py-5 transition-shadow hover:shadow-lg border">
-
-
-
-
-            {/* {product?.images?.map((image) => (
-              <img
-                key={image.id}
-                src={image.url}
-                alt={image.url}
-                className="w-full transition scale-[95%] hover:scale-100 delay-300"
-              />
-            ))} */}
-
-              <img src={product.images[0].url} alt="" />
-
-
-            <h2 className="pt-4">{product.title}</h2>
-            <h2 className="pt-4">{product.price}</h2>
-            <p className="mt-3">
-              <button className="bg-blue-600 text-white px-4 py-2 rounded-md transition-all hover:shadow-md scale-[95%] hover:scale-100">
-                Add to Cart
-              </button>
-            </p>
-          </div>
-        ))}
+        <section className="w-fit mx-auto grid grid-cols-1 lg:grid-cols-3 md:grid-cols-2 justify-items-center justify-center gap-y-20 gap-x-14 mt-10 mb-5">
+          {products.map((product) => (
+            <div key={product.id} className="w-72 bg-white shadow-md rounded-xl duration-500 hover:scale-105 hover:shadow-xl">
+              <Link to="#">
+                <img src={product.images[0]?.url || "default-image-url.jpg"} alt={product.title} className="h-80 w-72 object-cover rounded-t-xl" />
+                <div className="px-4 py-3 w-72">
+                  <span className="text-gray-400 mr-3 uppercase text-xs">Brand</span>
+                  <p className="text-lg font-bold text-black truncate block capitalize">{product.title}</p>
+                  <div className="flex items-center">
+                    <p className="text-lg font-semibold text-black cursor-auto my-3">${product.price}</p>
+                    <del>
+                      <p className="text-sm text-gray-600 cursor-auto ml-2">$199</p>
+                    </del>
+                    <div className="ml-auto">
+                      <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-bag-plus" viewBox="0 0 16 16">
+                        <path fillRule="evenodd" d="M8 7.5a.5.5 0 0 1 .5.5v1.5H10a.5.5 0 0 1 0 1H8.5V12a.5.5 0 0 1-1 0v-1.5H6a.5.5 0 0 1 0-1h1.5V8a.5.5 0 0 1 .5-.5z" />
+                        <path d="M8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1zm3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4h-3.5zM2 5h12v9a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V5z" />
+                      </svg>
+                    </div>
+                  </div>
+                </div>
+              </Link>
+            </div>
+          ))}
+        </section>
       </div>
     </div></>
 
